9cd8a148534bcfad1167fd0d7749924dc1fdc935
ARu92 <amruizmartin92@gmail.com>
2019-01-10 14:21:37 +0100

    jq

--- ejercicios_jq/intro_JQ.js
+++ ejercicios_jq/intro_JQ.js
@@ -1,11 +1,16 @@
-var listaPersonas = new Array();
+var listaPersonas
+
+jQuery().ready(function(){
+  listaPersonas = new Array();
+  jQuery("*").css("border-color", "red");
+});
 
 function addPersona(){
   var nuevaPersona = new Object();
-  nuevaPersona.nombre = jQuery("#inputNombre").val();
-  nuevaPersona.edad = jQuery("#inputEdad").val();
+  nuevaPersona.nombre = $("#inputNombre").val();
+  nuevaPersona.edad = $("#inputEdad").val();
   nuevaPersona.edad = parseInt(nuevaPersona.edad);
-  nuevaPersona.email = jQuery("inputEmail").val();
+  nuevaPersona.email = $("#inputEmail").get(0).value;
 
   listaPersonas[listaPersonas.length] = nuevaPersona;
   alert("persona creada:\n"
@@ -15,12 +20,12 @@ function addPersona(){
 }
 
 function listarPersonas(){
-  var divListado = document.getElementById("divListado");
-  divListado.innerHTML = "<h2>Lista de personas</h2>";
+  $("#divListado").html("<h2>Lista de personas</h2>");
 
     for(var i = 0; i < listaPersonas.length; i++){
     var persona = listaPersonas[i];
-    divListado.innerHTML += "<p>Nombre: "+ persona.nombre +", Edad: "
-    + persona.edad +", &lt;" + persona.email+"&gt;</p>";
+      $("#divListado").html(
+      $("#divListado").html()+"<p>Nombre: " + persona.nombre + ", Edad: "
+      + persona.edad +", &lt;" + persona.email+"&gt;</p>");
   }
 }
