64676967d8d41a55fb331dd09b2a055fb1287350
ARu92 <amruizmartin92@gmail.com>
2019-02-11 14:12:55 +0100

    Node JS

--- NodeJS/node_express.js
+++ NodeJS/node_express.js
@@ -7,5 +7,9 @@ var alResponder = (request, response) => {
   response.send("Ruta: " + request.originalUrl + "  Verbo: " + request.method);
 }
 app.get("/", alResponder);
+app.post("/", alResponder);
+app.delete("/", alResponder);
+app.all("/paratodos", alResponder);
+app.all("/enotrolado", alResponder);
 app.listen(9000);
 console.log("Servidor express lanzado."); 
