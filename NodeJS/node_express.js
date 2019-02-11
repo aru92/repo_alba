55bd3f00b12117f5b4314538a642fcbb7c1e59a1
ARu92 <amruizmartin92@gmail.com>
2019-02-11 14:34:16 +0100

    +NodeJS

--- NodeJS/node_express.js
+++ NodeJS/node_express.js
@@ -2,9 +2,20 @@
 var miExpress = require("express");
 var app = miExpress();
 //function alResponder (request, response) con funciones anónimas de ES6.Función callback
+var reservas = {
+  "GET": function(){
+    return "['todas','las','reservas']";
+  },
+  "POST": function(reserva){
+    console.log("Añadir reserva " + reserva.id + " "+ reserva.nombre);
+  }
+}
 var alResponder = (request, response) => {
   console.log("Acceso a la petición, ", request.originalUrl);
-  response.send("Ruta: " + request.originalUrl + "  Verbo: " + request.method);
+  //response.send("Ruta: " + request.originalUrl + "  Verbo: " + request.method);
+  var metodo = reservas[request.method]; 
+  console.log(metodo());
+  response.send(metodo());
 }
 app.get("/", alResponder);
 app.post("/", alResponder);
